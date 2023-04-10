e64d114ed45ba4cca1e777c3ae1a03f79b63cf5e
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2023-04-10 17:36:32 +0100

    Add fixture example

--- tests/test_example_packages/test_exmaple.py
+++ tests/test_example_packages/test_exmaple.py
@@ -1,22 +1,29 @@
 import pytest
 from src.example_package.example import add_one
 
+@pytest.fixture
+def example_fixture():
+    print()
+    print("The function havinfg ficture signature will run before each test.")
+    return None
+
+
 @pytest.mark.add_number
-def test_with_positive_number():
+def test_with_positive_number(example_fixture):
     positive_number = 1
     result = add_one(positive_number)
     assert result == positive_number + 1
 
 
 @pytest.mark.add_number
-def test_with_negative_number():
+def test_with_negative_number(example_fixture):
     negative_number = -1
     result = add_one(negative_number)
     assert result == negative_number + 1
     
 
 @pytest.mark.add_number
-def test_with_zero():
+def test_with_zero(example_fixture):
     zero = 0
     result = add_one(zero)
     assert result == zero + 1
