ffe9b9ae4c414e43235f77e0d79af3c7fbf51716
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2023-04-10 18:40:56 +0100

    Add docstrings

--- src/example_package/example.py
+++ src/example_package/example.py
@@ -1,10 +1,20 @@
 from src.example_package.example_class import ExampleClass
 
 def add_one(number: int) -> int:
+    """Add one to a given number.
+
+    :param int number: whole number
+    :return int: given number plus one
+    """
     return number + 1
 
 
 def add_one_with_example_class(number: int) -> int:
+    """Add one to a given number using ExampleClass.
+
+    :param int number: whole number
+    :return int: given number plus one
+    """
     example_class = ExampleClass(number)
     example_class.number = add_one(example_class.number)
     return example_class.number
--- src/example_package/example_class.py
+++ src/example_package/example_class.py
@@ -1,13 +1,29 @@
 class ExampleClass():
+    """Hold one integer with type check.
+    """
+    
     def __init__(self, number: int):
+        """Set-up this cllass.
+
+        :param int number: a whole number
+        """
         self.number = number
     
     @property
     def number(self) -> int:
+        """Retunr the private member variable number.
+
+        :return int: private member variable number
+        """
         return self.__number
     
     @number.setter
     def number(self, new_number: int):
+        """Check the type of a given new_number and set it as the private member variable number.
+
+        :param int new_number: new number
+        :raises TypeError: if the type of a given new_number is not int
+        """
         if not isinstance(new_number, int):
             msg = f"The type of a given number must be int, but it is {type(new_number)}"
             raise TypeError(msg)
