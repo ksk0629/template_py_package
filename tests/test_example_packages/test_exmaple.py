80e29cbc88abb43ba5aa551015230f698f445b09
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2023-04-10 17:59:52 +0100

    Change the test style to using class

--- tests/test_example_packages/test_exmaple.py
+++ tests/test_example_packages/test_exmaple.py
@@ -1,29 +1,24 @@
 import pytest
 from src.example_package.example import add_one
 
-@pytest.fixture
-def example_fixture():
-    print()
-    print("The function havinfg ficture signature will run before each test.")
-    return None
+
+class TestAddNumber():
+    @pytest.mark.add_number
+    @pytest.mark.parametrize("positive_number, output",[(1, 2), (2, 3), (3, 4), (5, 6)])
+    def test_with_positive_numbers(self, positive_number, output):
+        result = add_one(positive_number)
+        assert result == output
 
 
-@pytest.mark.add_number
-@pytest.mark.parametrize("number, output",[(1, 2), (2, 3), (3, 4), (5, 6)])
-def test_with_positive_numbers(example_fixture, number, output):
-    result = add_one(number)
-    assert result == output
+    @pytest.mark.add_number
+    @pytest.mark.parametrize("negative_number, output",[(-4, -3), (-3, -2), (-2, -1), (-1, 0)])
+    def test_with_negative_number(self, negative_number, output):
+        result = add_one(negative_number)
+        assert result == output
+        
 
-
-@pytest.mark.add_number
-@pytest.mark.parametrize("number, output",[(-4, -3), (-3, -2), (-2, -1), (-1, 0)])
-def test_with_negative_number(example_fixture, number, output):
-    result = add_one(number)
-    assert result == output
-    
-
-@pytest.mark.add_number
-def test_with_zero(example_fixture):
-    zero = 0
-    result = add_one(zero)
-    assert result == zero + 1
+    @pytest.mark.add_number
+    def test_with_zero(self):
+        zero = 0
+        result = add_one(zero)
+        assert result == zero + 1
