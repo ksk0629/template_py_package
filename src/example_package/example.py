c50b17f2d491ea4e723bea392c7eaadd97463cde
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2023-04-10 18:25:49 +0100

    Add a function without test for seeing coverage

--- src/example_package/example.py
+++ src/example_package/example.py
@@ -1,2 +1,10 @@
+from src.example_package.example_class import ExampleClass
+
 def add_one(number: int) -> int:
     return number + 1
+
+
+def add_one_with_example_class(number: int) -> int:
+    example_class = ExampleClass(number)
+    example_class.number = add_one(example_class.number)
+    return example_class.number
