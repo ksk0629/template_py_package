7aeaf82b17b0c0bbac652ea2f9dbb10166d38f70
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2023-09-16 16:37:31 +0100

    Add main function for script

--- src/example_package/example.py
+++ src/example_package/example.py
@@ -18,3 +18,7 @@ def add_one_with_example_class(number: int) -> int:
     example_class = ExampleClass(number)
     example_class.number = add_one(example_class.number)
     return example_class.number
+
+
+def main():
+    print(add_one(2))
